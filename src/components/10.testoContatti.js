6e6ef144184a9dc69c9ba0d610c965c101f55252
Alessandro <acalasanzio@outlook.com>
2022-03-11 17:18:58 +0100

    Sist contatti

--- src/components/10.testoContatti.js
+++ src/components/10.testoContatti.js
@@ -19,46 +19,51 @@ class TestoContatti extends React.Component {
     render() {
         return (
 
-       
+
             <Table borderless data-aos="fade-right" data-aos-mirror='true' data-aos-once='false'  >
-             
-                    <tr className="Asstr" >
-                        <th class="photo-grid1 card1-tall card1-wide">
-                            <div className="card2 imgSize1 sfondoContatti1">Via Rosolino Pilo,20 - Milano
-                            </div>
+
+                <tr className="Asstr" >
+                    <th class="photo-grid1 card1-tall card1-wide">
+                        <div className="card2 imgSize1 sfondoContatti1">Via Rosolino Pilo,20 - Milano
+                        </div>
+                    </th>
+                </tr>
+
+                <tr>
+                    <th className="photo-grid1 card1-tall card1-wide">
+                        <div className="card3 imgSize1 sfondoContatti2">Lunedi Domenica:<br /> 12:00/14:30 - 19:30/23:00
+                        </div>
+                    </th>
+                </tr>
+
+                <tr>
+                    <th class=" photo-grid1 card1-tall card1-wide">
+                        <div className="card3 imgSize1 sfondoContatti3" >Per info e prenotazioni: <br />
+                            <div style={{ color: "#9B111E" }}>02 2940 6526</div>
+                        </div>
+                    </th>
+                </tr>
+
+                <tr className="Asstr">
+            
+                    <div className="photo-grid1 card1-tall card1-wide card4 imgSize1 ">
+                        <th className="sfondoContatti5">
+                            <a class="fb1" href="https://it-it.facebook.com/angolodabruzzo1/">FACEBOOK</a>
                         </th>
-                    </tr>
-                   
-                    <tr>
-                        <th className="photo-grid1 card1-tall card1-wide">
-                            <div className="card3 imgSize1 sfondoContatti2">Lunedi Domenica:<br /> 12:00/14:30 - 19:30/23:00
-                            </div>
+                        <th className="sfondoContatti6">
+                            <a class="ig1" href="https://www.instagram.com/angolodabruzzo/">INSTAGRAM</a>
                         </th>
-                    </tr>
-
-                    <tr>
-                        <th class=" photo-grid1 card1-tall card1-wide">
-                            <div className="card3 imgSize1 sfondoContatti3" >Per info e prenotazioni: <br />
-                                <div style={{ color: "#9B111E" }}>02 2940 6526</div>
-                            </div>
+                        <th className="sfondoContatti7">
+                            <a class="tree1" href="https://linktr.ee/AngoloDabruzzo">LINKTREE</a>
                         </th>
-                    </tr>
 
-                    <tr className="Asstr">
-                        <th class=" photo-grid1 card1-tall card1-wide">
-                            <div className="card4 imgSize1 sfondoContatti4">I NOSTRI SOCIAL <br />
-                             <a  href="https://it-it.facebook.com/angolodabruzzo1/"><img src={facebook} alt="Angolo d'Abruzzo" title="Angolo d'Abruzzo" width="40px" height="40px" /></a>
-                             <a  href="https://www.instagram.com/angolodabruzzo/"><img src={instagram} alt="Angolo d'Abruzzo" title="Angolo d'Abruzzo" width="50px" height="50px" /></a>
-                             <a  href="https://linktr.ee/AngoloDabruzzo"><img src={linktree} alt="Angolo d'Abruzzo" title="Angolo d'Abruzzo" width="40px" height="40px" /></a> 
-
-                            </div>
-                        </th>
-                    </tr>
+                    </div>
+                </tr>
 
 
-           
+
             </Table>
-        
+
         )
     }
 }
